d51ee6ca35c2e3d49df66ba21f7470415a8f0d64
indiesurya18-073 <71679006+indiesurya18-073@users.noreply.github.com>
2020-12-17 22:36:46 +0800

    Update shop.php

--- shop.php
+++ shop.php
@@ -83,108 +83,17 @@ include 'navbar.php';
                 } ?>
             </div>
         </div>
-
     </div>
-
-
-    <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
-    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-    -->
-
     <!--Footer -->
     <br><br>
-    <footer class="page-footer font-small bg-dark">
-
-
-        <!-- Footer Links -->
-        <div class="container text-center text-md-left pt-4 pt-md-5">
-
-            <!-- Grid row -->
-            <div class="row mt-1 mt-md-0 mb-4 mb-md-0 text-muted">
-
-                <!-- Grid column -->
-                <div class="col-md-3 mx-auto mt-3 mt-md-0 mb-0 mb-md-4">
-
-                    <!-- Links -->
-                    <h5>Camshot</h5>
-                    <hr class="color-primary mb-4 mt-0 d-inline-block mx-auto w-60">
-                    <ul class="list-unstyled foot-desc">
-                        <li class="mb-2">
-                            <a href="login.php">Login</a>
-                        </li>
-                        <li class="mb-2">
-                            <a href="register.php">Sign up</a>
-                        </li>
-                        <li class="mb-2">
-                            <a href="#">Keranjang</a>
-                        </li>
-                    </ul>
-
-                </div>
-                <!-- Grid column -->
-
-                <hr class="clearfix w-100 d-md-none">
-
-                <!-- Grid column -->
-                <div class="col-md-3 mx-auto mt-3 mt-md-0 mb-0 mb-md-4">
-
-                    <!-- Links -->
-                    <h5 href="#">About</h5>
-                    <hr class="color-primary mb-4 mt-0 d-inline-block mx-auto w-60">
-                    <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aliquam quidem animi quia illo hic iure, cum, voluptate ipsa praesentium accusantium omnis maxime sunt commodi ipsam facere excepturi sit at?</p>
-
-
-                </div>
-                <!-- Grid column -->
-
-                <hr class="clearfix w-100 d-md-none">
-
-                <!-- Grid column -->
-                <div class="col-md-3 mx-auto mt-3 mt-md-0 mb-0 mb-md-4">
-
-                    <!-- Links -->
-                    <h5>Contacts</h5>
-                    <hr class="color-primary mb-4 mt-0 d-inline-block mx-auto w-60">
-
-                    <ul class="fa-ul foot-desc ml-4">
-                        <li class="mb-2"><span class="fa-li"><i class="fa fa-map"></i></span>Jalan Pantai Purnama No 19
-                        </li>
-                        <li class="mb-2"><span class="fa-li"><i class="fa fa-phone"></i></span>042 876 836 908</li>
-                        <li class="mb-2"><span class="fa-li"><i class="fa fa-envelope"></i></span>Camshot@gmail.com</li>
-                    </ul>
-                    <hr class="color-primary mb-4 mt-0 d-inline-block mx-auto w-60">
-                    <h5>Find us on</h5>
-                    <hr class="color-primary mb-4 mt-0 d-inline-block mx-auto w-60">
-                    <a style="font-size:20px;margin-right:5px;margin-bottom:20px;" href="#" class="fa fa-twitter"></a>
-                    <a style="font-size:20px;margin-right:5px;margin-bottom:20px;" href="#" class="fa fa-google"></a>
-                    <a style="font-size:20px;margin-right:5px;margin-bottom:20px;" href="#" class="fa fa-instagram"></a>
-
-                </div>
-                <!-- Grid column -->
-
-            </div>
-            <!-- Grid row -->
-
-        </div>
-        <!-- Footer Links -->
-
-        <!-- Copyright -->
-        <div class="footer-copyright text-center py-3 text-muted">© 2020 Copyright:
-            <a href="#"> Camshot</a>
-        </div>
-        <!-- Copyright -->
-
-    </footer>
-
+    <?php 
+        include 'footer.php';
+     ?>
 
 </body>
-
 </html>
