3f7f05a4f2eb70c94503941119f409fa23c37448
indiesurya18-073 <71679006+indiesurya18-073@users.noreply.github.com>
2020-12-17 22:39:32 +0800

    Update login.php

--- login.php
+++ login.php
@@ -8,56 +8,28 @@ include 'navbar.php';
 if (isset($_SESSION["user"])) header("Location: index.php");
 if (isset($_POST['login'])) {
 
-	$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
-	$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+	$ambil = $koneksi->query("SELECT * FROM user WHERE username='$_POST[username]' AND password='$_POST[password]'");
+	$akunyangcocok = $ambil->num_rows;
 
-	$sql = "SELECT * FROM user WHERE username=:username OR email=:email";
-	$stmt = $db->prepare($sql);
-
-	// bind parameter ke query
-	$params = array(
-		":username" => $username,
-		":email" => $username
-	);
-
-	$stmt->execute($params);
-	//eksekusi query
-	$user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-	// jika user terdaftar
-	if ($user) {
-		// verifikasi password
-		if (password_verify($password, $user["password"])) {
-			// buat Session
-			session_start();
-			// login sukses, alihkan ke halaman timeline
-			if ($user['level'] == "admin") {
-				$_SESSION["admin"] = $user;
-				header("Location: admin/index.php?pesan=admin");
-			}
-			if ($user['level'] == "user") {
-				$_SESSION["user"] = $user;
-
-				header("Location: index.php?pesan=user");
-			}
-		}
-	} else {
-		header("location:login.php?pesan=gagal");
-	}
-}
+	if($akunyangcocok==1){
+        $akun= $ambil->fetch_assoc();
+        $_SESSION['user'] = $akun;
+        echo "<script>alert('Login Berhasil');</script>";
+        if(isset($_SESSION['keranjang']) OR !empty($_SESSION['keranjang'])){
+        echo "<script>location='checkout.php'</script>";
+      }
+      else{
+        echo "<script>location='index.php'</script>";
+      }
+    }
+    else{
+        echo "<script>alert('Anda Gagal Login');</script>";
+        echo "<script>location='login.php'</script>";
+    }
+     }
 ?>
 
 <body>
-
-	<?php
-	if (isset($_GET['pesan'])) {
-		if ($_GET['pesan'] == "gagal") {
-			echo "<div class='alert alert-danger'> Username dan Password tidak sesuai !</div>";
-		}
-	}
-
-	?>
-
 	<br>
 	<div class="card mx-auto d-block" style="width: 520px; height: 350px;">
 		<div class="card mx-auto d-block border-0" style="width: 500px; height: 330px;">
@@ -80,21 +52,7 @@ if (isset($_POST['login'])) {
 	</div>
 
 	<script>
-		/*
-		let postBtn = document.querySelector('.post-btn'); //untuk input btn
-		let wrapper = document.querySelector('.create-post-wrapper'); //untuk wrap div
-		let inputs = [...wrapper.querySelectorAll('.post-input')]; //untuk semua input field
-
-		function validate() {
-			let isIncomplete = inputs.some(input => !input.value);
-			postBtn.disabled = isIncomplete;
-			postBtn.style.cursor = isIncomplete ? 'not-allowed' : 'pointer';
-		}
-
-		wrapper.addEventListener('input', validate);
-		validate();
-		*/
-
+	
 		function validasi() {
 			if (document.forms["register"]["username"].value != "") {
 				if (document.forms["register"]["password"].value != "") {
