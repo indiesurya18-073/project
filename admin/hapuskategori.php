efe05cf7088a1cb9a1d713f916e6421e570bfd31
indiesurya18-073 <71679006+indiesurya18-073@users.noreply.github.com>
2020-12-18 21:53:32 +0800

    Update hapuskategori.php

--- admin/hapuskategori.php
+++ admin/hapuskategori.php
@@ -4,3 +4,5 @@ include 'akses.php';
 $db->query("DELETE FROM kategori WHERE id_kategori='$_GET[id]'");
 echo "<script>alert('Kategori Terhapus');</script>";
 echo "<script>location='index.php?halaman=kategori';</script>";
+
+?>
