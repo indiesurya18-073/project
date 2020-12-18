541a0743a16b21666599278f942631e3879f7a78
indiesurya18-073 <71679006+indiesurya18-073@users.noreply.github.com>
2020-12-18 21:51:57 +0800

    Update edituser.php

--- admin/edituser.php
+++ admin/edituser.php
@@ -6,8 +6,8 @@ include 'akses.php';
         <h2>Edit User</h2>
     </div>
     <div class="panel-body">
-        <?php $ambil = $db->query("SELECT * FROM user WHERE id_user='$_GET[id]'");
-        $pecah = $ambil->fetch(PDO::FETCH_ASSOC); ?>
+        <?php $ambil = $koneksi->query("SELECT * FROM user WHERE id_user='$_GET[id]'");
+        $pecah = $ambil->fetch_assoc(); ?>
         <form method="post">
             <div class="form-group">
                 <label for="nama_user">Nama</label>
@@ -38,7 +38,7 @@ include 'akses.php';
     </div>
 </div>
 <?php if (isset($_POST["simpan"])) {
-    $db->query("UPDATE user SET name='$_POST[nama]',username='$_POST[username]',password='$_POST[password]',email='$_POST[email]',tlp_user='$_POST[tlp]',alamat_user='$_POST[alamat]' WHERE id_user='$_GET[id]'");
+    $koneksi->query("UPDATE user SET name='$_POST[nama]',username='$_POST[username]',password='$_POST[password]',email='$_POST[email]',tlp_user='$_POST[tlp]',alamat_user='$_POST[alamat]' WHERE id_user='$_GET[id]'");
     echo "<script>alert('User berhasil dirubah');</script>";
     echo "<script>location='index.php?halaman=user';</script>";
 }
