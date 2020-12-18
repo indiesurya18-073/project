4c1a0044a3cc9e630ed21798990b754aa3c0422e
indiesurya18-073 <71679006+indiesurya18-073@users.noreply.github.com>
2020-12-18 21:56:21 +0800

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -3,7 +3,8 @@ include 'akses.php';
 ?>
 <div class="panel-headline">
     <div class="panel-heading">
-        <h2>Halaman Home</h2>
+        <h2><center>SELAMAT DATANG ADMIN, SELAMAT BEKERJA DENGAN BAIK, TUHAN MENYERTAIMU</center></h2>
         <!-- <pre><?php print_r($_SESSION); ?></pre> -->
     </div>
 </div>
+?>
